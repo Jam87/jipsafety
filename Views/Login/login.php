c1398f06833c77ef712c6ac3c2b5f3249bd2e2e1
Javier Mora <jamj1702@yahoo.es>
2023-03-30 17:55:42 -0600

    favicon

--- Views/Login/login.php
+++ Views/Login/login.php
@@ -4,12 +4,12 @@
 <head>
 
     <meta charset="utf-8" />
-    <title>Sign Up | Velzon - Admin & Dashboard Template</title>
+    <title>Jipsafety - Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesbrand" name="author" />
     <!-- App favicon -->
-    <link rel="shortcut icon" href="public/images/favicon.ico">
+    <link rel="shortcut icon" href="public/images/favicon.png">
 
 
     <!-- Bootstrap Css -->
