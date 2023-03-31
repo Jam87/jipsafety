7e6ab3f865bd15e4534fcdd6fdb5f23bce8c4f8f
Javier Mora <jamj1702@yahoo.es>
2023-03-31 15:22:27 -0600

    cambio

--- Views/html/MainJs.php
+++ Views/html/MainJs.php
@@ -2,28 +2,66 @@
      //system url
      const base_url = "<?= base_url(); ?>";
  </script>
- <!-- JAVASCRIPT -->
+
  <script src="<?= base_url(); ?>/public/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
  <script src="<?= base_url(); ?>/public/lib/simplebar/simplebar.min.js"></script>
- <script src="<?= base_url(); ?>/public/li/node-waves/waves.min.js"></script>
- <script src="<?= base_url(); ?>/public/libs/feather-icons/feather.min.js"></script>
+ <script src="<?= base_url(); ?>/public/lib/node-waves/waves.min.js"></script>
+ <script src="<?= base_url(); ?>/public/lib/feather-icons/feather.min.js"></script>
  <script src="<?= base_url(); ?>/public/js/pages/plugins/lord-icon-2.1.0.js"></script>
  <script src="<?= base_url(); ?>/public/js/plugins.js"></script>
 
- <!-- apexcharts -->
- <script src="<?= base_url(); ?>/public/libs/apexcharts/apexcharts.min.js"></script>
+ 
+ <!--Invoice create init js-->
+ <script src="<?= base_url(); ?>/public/js/pages/invoicecreate.init.js"></script>
 
- <!-- Vector map-->
+ <!-- apexcharts -->
+ <script src="<?= base_url(); ?>/public/lib/apexcharts/apexcharts.min.js"></script>
+ 
+ <!-- Vector map -->
  <script src="<?= base_url(); ?>/public/lib/jsvectormap/js/jsvectormap.min.js"></script>
  <script src="<?= base_url(); ?>/public/lib/jsvectormap/maps/world-merc.js"></script>
 
+  <!-- choices.min.js -->
+  <script src="<?= base_url(); ?>/public/lib/choices.js/public/assets/scripts/choices.min.js"></script>
+
  <!--Swiper slider js-->
  <script src="<?= base_url(); ?>/public/lib/swiper/swiper-bundle.min.js"></script>
-
+ 
+ 
  <!-- Dashboard init -->
  <script src="<?= base_url(); ?>/public/js/pages/dashboard-ecommerce.init.js"></script>
+  
+ <script src="<?= base_url(); ?>/public/lib/select2/js/select2.min.js"></script>
 
- <!-- App js -->
+ <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+ <!-- particles js -->
+ <script src="<?= base_url(); ?>/public/lib/particles.js/particles.js"></script>
+ <!-- particles app js -->
+ <script src="<?= base_url(); ?>/public/js/pages/particles.app.js"></script>
+ <!-- validation init -->
+ <script src="<?= base_url(); ?>/public/js/pages/form-validation.init.js"></script>
+ <!-- password create init -->
+ <script src="<?= base_url(); ?>/public/js/pages/passowrd-create.init.js"></script>
+
+  <!-- App js -->
  <script src="<?= base_url(); ?>/public/js/app.js"></script>
+ 
+
+ <!-- Cargar DataTable, etc-->
+ <!--jQuery-->
+ 
+
+ <!--datatable js-->
+ <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+ <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
+ <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
+ <script src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+
+ <script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.print.min.js"></script>
+ <script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
 
  <script src="<?= media(); ?>/js/<?= $data['page_functions_js']; ?>"></script>
