fd5eefed10eefc6164fb21a06aa7cbd25be28204
Javier Mora <jamj1702@yahoo.es>
2023-03-31 14:17:00 -0600

    cambio

--- Views/html/MainJs.php
+++ Views/html/MainJs.php
@@ -2,13 +2,13 @@
      //system url
      const base_url = "<?= base_url(); ?>";
  </script>
- 
+
  <script src="<?= base_url(); ?>/public/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
  <script src="<?= base_url(); ?>/public/lib/simplebar/simplebar.min.js"></script>
  <script src="<?= base_url(); ?>/public/lib/node-waves/waves.min.js"></script>
  <script src="<?= base_url(); ?>/public/lib/feather-icons/feather.min.js"></script>
  <script src="<?= base_url(); ?>/public/js/pages/plugins/lord-icon-2.1.0.js"></script>
- <!--<script src="public/js/plugins.js"></script>-->
+ <script src="<?= base_url(); ?>/public/js/plugins.js"></script>
 
  
  
