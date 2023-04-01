c9def69c951eaa0de8abaec53f84b5518a3cf90a
Javier <jamj1702@yahoo.es>
2023-04-01 11:20:27 -0600

    se quito particles.app.js de MainJs

--- Views/html/MainJs.php
+++ Views/html/MainJs.php
@@ -32,8 +32,7 @@
 
  <!-- particles js -->
  <script src="<?= base_url(); ?>public/lib/particles.js/particles.js"></script>
- <!-- particles app js -->
- <script src="<?= base_url(); ?>public/js/pages/particles.app.js"></script>
+
  <!-- validation init -->
  <script src="<?= base_url(); ?>public/js/pages/form-validation.init.js"></script>
  <!-- password create init -->
