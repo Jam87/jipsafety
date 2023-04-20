8c78f8d4d4baea64d71f09b5e8d7be3373d3a757
Javier Mora <jamj1702@yahoo.es>
2023-04-19 21:33:11 -0600

    Vista compra

--- Views/html/MainMenu.php
+++ Views/html/MainMenu.php
@@ -52,7 +52,7 @@
         </li><!-- /Modulo: Clientes -->
 
         <!-- COMPRAS -->
-        <li class="nav-item">
+        <!--<li class="nav-item">
           <a class="nav-link menu-link" href="#sidebarIcons" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarIcons">
             <i class="ri-compasses-2-line"></i> <span data-key="t-icons">Compras</span>
           </a>
@@ -62,10 +62,10 @@
                 <a href="<?= base_url(); ?>proveedores" class="nav-link" data-key="t-remix">Proveedores</a>
               </li>
               <li class="nav-item">
-
+                <a href="<?= base_url(); ?>Compras" class="nav-link" data-key="t-boxicons">Compras</a>
               </li>
 
-        </li>
+        </li>-->
       </ul>
     </div>
     </li>
